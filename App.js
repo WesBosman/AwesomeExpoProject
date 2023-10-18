5d6de76218692d96da687ae563571b60a6749d29
Wes Bosman <bosmanwn@vcu.edu>
2023-10-18 09:37:12 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Hyperview from 'hyperview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <WebView
+    //   style={styles.container}
+    //   source={{
+    //     uri: 'https://66a1-2600-4040-115b-0-21a1-f60e-9a22-1b81.ngrok-free.app/src/static/hello-world.xml',
+    //   }}
+    // />
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <Hyperview
+      fetch={fetch}
+      entrypointUrl={'http://localhost:3000/src/static/hello-world.xml'}
+    ></Hyperview>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
+    height: '100%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
---
+++ README.md
@@ -0,0 +1,8 @@
+### Example using hyperscript and htmx
+
+To run the react native app
+
+```
+cd AwesomeExpoProject
+nvm use; yarn; npx expo start
+```
